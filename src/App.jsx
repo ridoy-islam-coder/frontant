a650b1f6d98a16200ca94441cc53e23ca52d277d
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2026-01-22 02:19:02 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Demo from './Demo';
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
       <p>Welcome to my React application!</p>
+
+
+      <div>
+
+
+        <h2>This is a Demo Component</h2>
+         <Demo  />
+      </div>
 
     </div>
   );
---
+++ src/Demo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+            <h2>This is a Demo Component</h2>
+            <p>It serves as an example of a React functional component.</p>
+            <input type="text" /> 
+            <button>Click Me</button>
+        </div>
+    );
+};
+
+export default Demo;
