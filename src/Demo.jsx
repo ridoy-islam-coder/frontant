5f399abe9bca7609fe1599a7d4edba5a968ca6f4
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2026-01-22 23:41:28 +0600

    done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
 const Demo = () => {
+
+    const city = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+
+
     return (
         <div>
             <h2>This is a Demo Component</h2>
             <p>It serves as an example of a React functional component.</p>
             <input type="text" /> 
             <button>Click Me</button>
+
+      <div>
+
+        <h3>City List:</h3>
+        <ul>
+        {
+         city.map((cityName, index) =>
+            
+            <li key={index}>{cityName}</li>
+        
+        )
+
+        }
+
+        </ul>
+
+
+      </div>
+
         </div>
     );
 };
